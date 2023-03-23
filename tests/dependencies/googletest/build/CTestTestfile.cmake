b033ee01858151190aa86bae82871a1486ef0899
Hallis Regina <hregina@student.21-school.ru>
2023-03-23 17:48:14 +0400

    контейнеры

--- tests/dependencies/googletest/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/elen/21school/A2_SimpleNavigator_v1.0-0/tests/dependencies/googletest
-# Build directory: /Users/elen/21school/A2_SimpleNavigator_v1.0-0/tests/dependencies/googletest/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
